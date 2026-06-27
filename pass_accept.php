194df73c8a475210b9b8e5dbb50e2f89fbb7d9d4
codimic <75594320+codimic@users.noreply.github.com>
2026-06-27 12:43:14 +0200

    pass_accept review and html file add

--- pass_accept.php
+++ pass_accept.php
@@ -1,83 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$pws = isset($_POST["pws"]) ? trim($_POST["pws"]) : "";
+$srt = isset($_POST["srt"]) ? trim($_POST["srt"]) : "";
+$fName = isset($_POST["fName"]) ? trim($_POST["fName"]) : "";
 
+$expectedPws = base64_decode("VGgxNV8xNV81VFIwbjY");
+$isValid = ($pws === $expectedPws && $srt === "1352" && $fName !== "");
+?>
+<!DOCTYPE html>
+<html lang="pl">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>HackerU</title>
-  <!-- Bootstrap core CSS -->
+  <title>Pass Accept</title>
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-  <!-- Custom styles for this template -->
-  <link href="css/clean-blog.min.css" rel="stylesheet">
 </head>
-
-<body style="background-image: url('img/background.jfif'); background-size:100% 100%">
-  <!-- Page Header -->
+<body style="background-image: url('img/background.jfif'); background-size: cover; min-height: 100vh;">
   <div class="container py-5">
-      <div class="col-12 col-md-8 col-lg-6 mx-auto">
-          <div style="background-color: rgb(255,255,255,0.4)" class="p-3 shadow">
-            <?php
-                $pws = isset($_POST["pws"]) ? $_POST["pws"] : "";
-                $srt = isset($_POST["srt"]) ? $_POST["srt"] : "";
-                $fName = isset($_POST["fName"]) ? $_POST["fName"] : "";
-                
-                if ($pws === base64_decode("VGgxNV8xNV81VFIwbjY") && $srt === "1352" && !empty($fName)) {
-                  ?>
-                  <script>
-                    var div_1 = document.querySelector("#d1");
-                    var div_2 = document.querySelector("#d2");
-                    var header = document.querySelector("#h");
-                    
-                    var div_style = window.getComputedStyle(div_1);
-                    var header_style = window.getComputedStyle(header);
-                
-                    function A1() {
-                        if (div_1.children[0].nodeName === "DIV") {
-                            console.log("You nailed it !");
-                            if (div_2.children[0].nodeName === "H1") {
-                                div_2.children[0].innerHTML = "This is correct too!";
-                                A3();
-                            }
-                        }
-                    }
-                    
-                    function A2(params) {
-                        if ((params / (10 % 4)) === 132.993) {
-                            var m = document.querySelector("Math");
-                            m.innerHTML = "Set Text In Here :";
-                            var arr = [m];
-                            arr.push("Element");
-                            arr.push("Second Element");
-                            m.addEventListener("click", function() {
-                                A2(arr);
-                            });
-                        }
-                    }
-                    
-                    function A3() {
-                        if (div_style["textAlign"] === "center" && div_style["fontFamily"] === "fantasy") {
-                            console.log("Just one more step");
-                            if (header_style.color === "rgb(255, 99, 71)" && header_style.transform === "matrix(-1, 0, 0, -1, 0, 0)") {
-                                alert("AMAZING YOU DID IT !!!");
-                            }
-                        }
-                    }
-                    
-                    A1();
-                  </script>
-                  <?php
-                }
-                ?>
-          </div>
-      </div>      
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-8 col-lg-6">
+        <div class="p-4 shadow rounded" style="background-color: rgba(255,255,255,0.92);">
+          <?php if ($isValid): ?>
+            <h1 class="h4 mb-3">Dziękujemy, <?php echo htmlspecialchars($fName, ENT_QUOTES, 'UTF-8'); ?>!</h1>
+            <p class="mb-0">Weryfikacja danych zakończyła się sukcesem.</p>
+          <?php else: ?>
+            <h1 class="h4 mb-3">Błąd walidacji</h1>
+            <p class="mb-0">Wprowadzone dane są nieprawidłowe.</p>
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
   </div>
-
 </body>
-
 </html>
